4fbb96eedd0f39f6ba754fb0df7b3b3ff717b965
mcgear <michael.gearhardt@fathym.com>
2019-04-11 17:26:04 -0600

    Why won't it bring these files

--- projects/common/src/lib/core/internal-wizard-question.model.ts
+++ projects/common/src/lib/core/internal-wizard-question.model.ts
@@ -1,44 +1,43 @@
 import { FormGroup } from '@angular/forms';
 
 export class InternalWizardQuestionModel {
+  /**
+   * The name of the form group
+   */
+  public FormGroup: FormGroup;
 
-    /**
-     * The name of the form group
-     */
-    public FormGroup: FormGroup;
+  /**
+   * THe name of the form control
+   */
+  public FormCtrl: string | string[];
 
-    /**
-     * THe name of the form control
-     */
-    public FormCtrl: string | string[];
+  /**
+   * The question to be displayed
+   */
+  public Question: string;
 
-    /**
-     * The question to be displayed
-     */
-    public Question: string;
+  /**
+   * The type of question
+   */
+  public Type: string;
 
-    /**
-     * The type of question
-     */
-    public Type: string;
+  /**
+   * Optional placeholder to display
+   */
+  public Placeholder: string;
 
-    /**
-     * Optional placeholder to display
-     */
-    public Placeholder: string;
+  /**
+   * Optional options to display
+   */
+  public Options: any[];
 
-    /**
-     * Optional options to display
-     */
-    public Options: any[];
+  /**
+   * Boolean to determine whether or not an answer is required or optional
+   */
+  public Required: boolean;
 
-    /**
-     * Boolean to determine whether or not an answer is required or optional
-     */
-    public Required: boolean;
-
-    /**
-     * Date range used for date range question types
-     */
-    public DateRange?: {};
+  /**
+   * Date range used for date range question types
+   */
+  public DateRange?: {};
 }
